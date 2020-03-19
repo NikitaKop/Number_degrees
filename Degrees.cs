5b1826da986776a739834bb54a40eeee999be380
Nikita <Nik2000kop@gmail.com>
2020-03-19 10:02:08 +0300

    Добавьте файлы проекта.

---
+++ Degrees.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_degree
+{
+    public static class Degrees
+    {
+        static double Degree_2 (double number)
+        {
+            Console.WriteLine(number * number);
+            return number * number;
+        }
+
+        static double Degree_3(double number)
+        {
+            Console.WriteLine(number * number * number);
+            return number * number * number;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_degree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
